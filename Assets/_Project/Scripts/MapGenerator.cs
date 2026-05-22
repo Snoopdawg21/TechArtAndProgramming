c274be00e8b9288b0009aba9a56eb753437a93fe
Snoopdawg21 <65247774+Snoopdawg21@users.noreply.github.com>
2026-05-21 22:18:50 -0600

    Starting Procedural Generation
    Unity Crashed, i feel such joy and much happiness!! 😄

--- Assets/_Project/Scripts/MapGenerator.cs
+++ Assets/_Project/Scripts/MapGenerator.cs
@@ -2,15 +2,40 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private int gridSize;
+    
+    [Space(10)]
+    [Header("Hallway Information")]
+    [SerializeField] private Vector3Int[] hallWorldPositions;
+    [SerializeField] private int[,] hallGridPositions;
+    
+    [Space(10)]
+    [Header("Room Information")]
+    [SerializeField] private Vector3Int[] roomWorldPositions;
+    [SerializeField] private int[,] roomGridPositions;
+    [SerializeField] private int numberOfRooms;
+    
     void Start()
+    {
+        GenerateHallways();
+    }
+
+    
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GenerateHallways()
     {
-        
+        numberOfRooms = Random.Range(3, 10);
+        roomWorldPositions = new Vector3Int[numberOfRooms];
+        roomGridPositions = new int[numberOfRooms, numberOfRooms];
+
+        for (int i = 0; i < numberOfRooms; i++)
+        {
+            roomGridPositions[i , i] = Random.Range(0, gridSize);
+            Debug.Log(roomGridPositions[i , i]);
+        }
     }
 }
