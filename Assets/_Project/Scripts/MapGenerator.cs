abb6de1fe528900624e35b3ac026c1cbdf29a8d0
Snoopdawg21 <65247774+Snoopdawg21@users.noreply.github.com>
2026-05-22 11:41:02 -0600

    corners spawning!!
    yippee!!!

--- Assets/_Project/Scripts/MapGenerator.cs
+++ Assets/_Project/Scripts/MapGenerator.cs
@@ -7,17 +7,23 @@ public class MapGenerator : MonoBehaviour
     [Space(10)]
     [Header("Hallway Information")]
     [SerializeField] private Vector3Int[] hallWorldPositions;
-    [SerializeField] private int[,] hallGridPositions;
+    [SerializeField] private Vector2Int[] hallGridPositions;
     
     [Space(10)]
     [Header("Room Information")]
     [SerializeField] private Vector3Int[] roomWorldPositions;
-    [SerializeField] private int[,] roomGridPositions;
+    [SerializeField] private Vector2Int[] roomGridPositions;
     [SerializeField] private int numberOfRooms;
+
+    [Space(10)] 
+    [Header("Prefabs")]
+    [SerializeField] private GameObject hallwayPrefab;
+
+    [SerializeField] private GameObject cornerPrefab;
     
     void Start()
     {
-        GenerateHallways();
+        GenerateRooms();
     }
 
     
@@ -26,16 +32,37 @@ public class MapGenerator : MonoBehaviour
         
     }
 
-    private void GenerateHallways()
+    private void GenerateRooms()
     {
         numberOfRooms = Random.Range(3, 10);
         roomWorldPositions = new Vector3Int[numberOfRooms];
-        roomGridPositions = new int[numberOfRooms, numberOfRooms];
+        roomGridPositions = new Vector2Int[numberOfRooms];
 
         for (int i = 0; i < numberOfRooms; i++)
         {
-            roomGridPositions[i , i] = Random.Range(0, gridSize);
-            Debug.Log(roomGridPositions[i , i]);
+            roomGridPositions[i].x = Random.Range(0, gridSize);
+            roomGridPositions[i].y = Random.Range(0, gridSize);
+
+            roomWorldPositions[i].x = roomGridPositions[i].x * 4;
+            roomWorldPositions[i].z = roomGridPositions[i].y * 4;
+            
+            Instantiate(hallwayPrefab, roomWorldPositions[i], Quaternion.identity);
+        }
+        
+        FindCorners();
+    }
+
+    private void FindCorners()
+    {
+        hallGridPositions = new Vector2Int[numberOfRooms];
+        hallWorldPositions = new Vector3Int[numberOfRooms];
+        
+        for (int i = 0; i < numberOfRooms - 1; i++)
+        {
+            hallGridPositions[i] = new Vector2Int(roomGridPositions[i].x, roomGridPositions[i+1].y);
+            hallWorldPositions[i] = new Vector3Int(hallGridPositions[i].x * 4, 0, hallGridPositions[i].y * 4);
+            
+            Instantiate(cornerPrefab, hallWorldPositions[i], Quaternion.identity);
         }
     }
 }
