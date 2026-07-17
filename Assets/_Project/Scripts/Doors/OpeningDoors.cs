9b3e163ab9f63a3cc66472564d64f165d7752f84
Snoopdawg21 <65247774+Snoopdawg21@users.noreply.github.com>
2026-07-17 15:18:13 -0600

    turns out animations was the answer
    it did not take me three hours to animate the door 😭 ik what this looks like i walked away for a bit i promsise

--- Assets/_Project/Scripts/Doors/OpeningDoors.cs
+++ Assets/_Project/Scripts/Doors/OpeningDoors.cs
@@ -3,43 +3,15 @@ using UnityEngine;
 public class OpeningDoors : MonoBehaviour
 {
     public int doorNum;
-    [SerializeField] private Transform doorBody;
-    [SerializeField] private float rotationSpeed;
-    private bool canTurn;
-
-    private void Update()
-    {
-        if (!canTurn) return;
-        Debug.Log(doorBody.localRotation.eulerAngles.y);
-        if (doorBody.localRotation.eulerAngles.y >= 90|| 
-            doorBody.localRotation.eulerAngles.y <= 0f)
-        {
-            canTurn = false;
-            return;
-        }
-        
-        RotateDoor();
-    }
-
-    private void RotateDoor()
-    {
-        doorBody.Rotate(0, rotationSpeed * Time.deltaTime, 0);
-    }
+    [SerializeField] private Animator anim;
     
     public void OpenDoor()
     {
-        Debug.Log("opening");
-        canTurn = true;
-        rotationSpeed *= -1;
-        RotateDoor();
-        doorBody.eulerAngles = new Vector3(0, doorBody.localRotation.eulerAngles.y + 0.5f, 0);
+        anim.SetBool("open", true);
     }
 
     public void CloseDoor()
     {
-        canTurn = true;
-        rotationSpeed *= -1;
-        RotateDoor();
-        doorBody.eulerAngles = new Vector3(0, doorBody.localRotation.eulerAngles.y - 0.5f, 0);
+        anim.SetBool("open", false);
     }
 }
